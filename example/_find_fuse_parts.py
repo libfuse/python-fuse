be44e3c94169b52927086758f695042a986223ea
dzsekijo <dzsekijo>
2006-10-12 23:53:50 +0000

    emit a more useful error message when examples fail to import fuse

--- example/_find_fuse_parts.py
+++ example/_find_fuse_parts.py
@@ -1,5 +1,6 @@
-import sys, os, glob 
+import sys, os, glob
 from os.path import realpath, dirname, join
+from traceback import format_exception
 
 ddd = realpath(join(dirname(sys.argv[0]), '..'))
 
@@ -12,8 +13,10 @@ try:
 except ImportError:
     raise RuntimeError, """
 
+! Got exception:
+""" + "".join([ "> " + x for x in format_exception(*sys.exc_info()) ]) + """
 ! Have you ran `python setup.py build'?
 !
 ! We've done our best to find the necessary components of the FUSE bindings
-! in an uninstalled state, but no dice...
+! even if it's not installed, we've got no clue what went wrong for you...
 """
