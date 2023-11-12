269413b12b5c48cac511d0ee3e9740671db05271
Sébastien Delafond <seb@debian.org>
2023-11-12 10:29:40 +0100

    Officially support up to python 3.12

--- setup.py
+++ setup.py
@@ -31,6 +31,9 @@ classifiers = [ "Development Status :: 5 - Production/Stable",
                 "Programming Language :: Python :: 3.7",
                 "Programming Language :: Python :: 3.8",
                 "Programming Language :: Python :: 3.9",
+                "Programming Language :: Python :: 3.10",
+                "Programming Language :: Python :: 3.11",
+                "Programming Language :: Python :: 3.12",
                 "Topic :: System :: Filesystems" ]
 
 # write default fuse.pc path into environment if PKG_CONFIG_PATH is unset
